2eab19f92ac5c69bf5deb3d3231aa22960032966
Ankit Verma <ankitv78389@gmail.com>
2024-05-22 23:34:58 +0530

    :hammer:[refractor][added color pallete and hex code]

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
-const displayDiv = document.querySelector(".display-color");
+const displayDiv = document.querySelector("#display-color");
 const inputColor = document.querySelector(".input-color");
-fetch(
-  `https://www.thecolorapi.com/scheme?hex=${inputColor}&format=json&mode=analogic&count=5`,
-  {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  }
-)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! status:${response.status}`);
-    }
+const button = document.querySelector(".btn");
+const selectedOption = document.getElementById("option-box");
 
-    return response.json();
-  })
-  .then((data) => console.log(data))
-  .catch((error) => console.error("Fetch error:", error));
+const imageEl = document.createElement("img");
+imageEl.classList = "image-theme";
+button.addEventListener("click", function () {
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${inputColor.value.slice(
+      1
+    )}&format=json&mode=${selectedOption.value}&count=5`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status:${response.status}`);
+      }
+
+      return response.json();
+    })
+    .then((data) => {
+      imageEl.src = data.image.bare;
+      displayDiv.appendChild(imageEl);
+      console.log(data);
+
+      const hexDiv = document.createElement("div");
+
+      data.colors.forEach((color) => {
+        const colorCodeEl = document.createElement("div");
+        colorCodeEl.classList.add("color-code");
+        colorCodeEl.textContent = color.hex.value;
+        hexDiv.appendChild(colorCodeEl);
+      });
+
+      displayDiv.appendChild(hexDiv);
+    })
+    .catch((error) => console.error("Fetch error:", error));
+  // console.log(inputColor.value);
+  // console.log(selectedOption.value);
+});
